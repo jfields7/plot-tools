e26245b41f4d7c3de941f03ebb8bcff322cdbf14
Jacob Fields <fields.jacobm@gmail.com>
2025-01-27 15:54:47 -0500

    Added metadata and license information.

---
+++ athplot/__init__.py
@@ -0,0 +1,9 @@
+"""
+AthPlot
+
+Utilities for manipulating and plotting AthenaK binary data
+"""
+
+__version__ = "0.1"
+__author__ = "Jacob Fields"
+__credits__ = "The Pennsylvania State University"
